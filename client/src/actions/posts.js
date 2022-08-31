07f0249f1bd265d363a175cf2369cc7ab4fdbdca
IvanDP12 <ikunev26@gmail.com>
2022-08-31 17:37:22 +0300

    fetchin posts successful

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -11,3 +11,13 @@ export const getPosts = () => async (dispatch) => {
     }
 
 }
+
+export const createPost = (post) => async (dispatch) => {
+    try {
+        const { data } = await api.createPost(post)
+
+        dispatch({ type: 'CREATE', payload: data})
+    } catch (error) {
+        console.log(error)
+    }
+}
